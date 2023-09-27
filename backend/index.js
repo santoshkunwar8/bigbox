d66e7f4a24304cb63e68f30569e1cedd5972e8e8
santosh <santehero8@gmail.com>
2023-09-27 16:44:16 +0545

    added fronend ulr

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const session = require("express-session")
 require("dotenv").config()
 app.use(cookieParser())
 
-app.use(cors({origin:"http://localhost:5173",methods:['GET','POST','DELETE','PUT'],credentials:true}))
+app.use(cors({origin:["http://localhost:5173",process.env.FRONTEND_URL],methods:['GET','POST','DELETE','PUT'],credentials:true}))
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ limit:'50mb', extended:true}))
 
