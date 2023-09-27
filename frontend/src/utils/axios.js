bf0404c0c4e71b40477ae6531a55209f921a401b
santosh <santehero8@gmail.com>
2023-09-27 16:42:02 +0545

    added backend link

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL:"https://wrapfile.onrender.com/api",
     withCredentials:true,
 })
 
