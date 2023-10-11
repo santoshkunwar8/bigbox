9b025f4d87fe655de60dd7b91ead95d62ea5e9dc
lovely boiii <94507951+Santoshkunwar44@users.noreply.github.com>
2023-10-11 13:57:38 +0545

    Update index.js
    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const session = require("express-session")
 require("dotenv").config()
 app.use(cookieParser())
 
-app.use(cors({origin:["http://localhost:5173",process.env.FRONTEND_URL],methods:['GET','POST','DELETE','PUT'],credentials:true}))
+app.use(cors({origin:["http://localhost:5173","http://127.0.0.1:5500",process.env.FRONTEND_URL],methods:['GET','POST','DELETE','PUT'],credentials:true}))
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ limit:'50mb', extended:true}))
 
