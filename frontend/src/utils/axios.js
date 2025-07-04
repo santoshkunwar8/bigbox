4dd862d62c34ec563c567ec5f10ab55e327bf2e1
Santosh Kunwar <santosh.dev300@gmail.com>
2025-07-04 00:53:45 +0000

    changed backend url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"https://wrapfile.onrender.com/api",
+    baseURL:"https://bigbox-rcg6.onrender.com/api",
     withCredentials:true,
 })
 
 export default axiosInstance;
-
-
